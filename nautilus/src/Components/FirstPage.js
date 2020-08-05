9319e8992e4331c8083afe27187dfd4c572b3688
SadafHaroon2020 <sadafssuet05@gmail.com>
2020-08-05 15:56:49 -0500

    working on Frist page and updating css

--- nautilus/src/Components/FirstPage.js
+++ nautilus/src/Components/FirstPage.js
@@ -7,11 +7,21 @@ import App from '../App.css'
 export default class FirstPage extends Component {
   render() {
     return (
-      <div  styles={MatchesImg} >
-         <div className="bg"></div>
+      <div>
+
+         <div className="bg">
          <div className="text">
+           <h4>Issue 079</h4>
+           <h2>Catalyst</h2>
+           <h5>Agents of change</h5>
+           <h4>Read More</h4>
+         </div>
+         <div className="_btn">
+         <button className = "btn">See Full Issue</button>
          </div>
       </div>
+      </div>
+      
     )
   }
 }
